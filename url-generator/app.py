9189827083be3e01e877b0514747a387923aad39
PinJhih <isaacwang29@gmail.com>
2025-03-30 15:16:26 +0800

    Add APIs for generating download URL and get file list

--- url-generator/app.py
+++ url-generator/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 from url_generator.minio_client import MinIOClient
 
@@ -9,6 +9,25 @@ minio_client = MinIOClient()
 @app.route("/", methods=["GET"])
 def index():
     return "File Download Service"
+
+
+@app.route("/files", methods=["GET"])
+def get_files():
+    try:
+        files = minio_client.file_list()
+        return jsonify(files)
+    except Exception as e:
+        app.logger.error(f"{e}")
+        return jsonify({"error": "Failed to get file list"}), 500
+
+
+@app.route("/url/<string:file_name>/<int:expiry>", methods=["GET"])
+def generate_url(file_name, expiry):
+    url = minio_client.generate_download_url(file_name, expiry)
+    if url:
+        return jsonify({"file_name": file_name, "url": url})
+    else:
+        return jsonify({"error": "Failed to generate presigned URL"}), 500
 
 
 if __name__ == "__main__":
--- url-generator/url_generator/minio_client.py
+++ url-generator/url_generator/minio_client.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 from minio import Minio
 from minio.error import S3Error
@@ -11,6 +12,19 @@ MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
 MINIO_BUCKET = os.getenv("MINIO_BUCKET", "file-download")
+
+
+def format_size(size_in_bytes):
+    units = ["B", "KiB", "MiB", "GiB", "TiB", "PiB"]
+    size = float(size_in_bytes)
+    unit_index = 0
+
+    while size >= 1024 and unit_index < len(units) - 1:
+        size /= 1024
+        unit_index += 1
+
+    return f"{size:.2f} {units[unit_index]}"
+
 
 class MinIOClient:
     def __init__(self):
@@ -30,9 +44,31 @@ class MinIOClient:
 
         except S3Error as e:
             logger.error(f"MinIO connection failed:\n{e}")
-            exit(1)
+            raise e
         except Exception as e:
             logger.exception(
                 f"An unexpected error occurred while connecting to MinIO:\n{e}"
             )
-            exit(1)
+            raise e
+
+    def file_list(self):
+        objects = self.minio_client.list_objects(MINIO_BUCKET, recursive=True)
+
+        # extract name, upload time and size, and format the fields
+        file_list = list(
+            map(
+                lambda obj: {
+                    "name": obj.object_name,
+                    "last_modify": obj.last_modified.strftime("%Y-%m-%d %H:%M:%S"),
+                    "size": format_size(obj.size),
+                },
+                objects,
+            )
+        )
+        return file_list
+
+    def generate_download_url(self, object_name, expiry):
+        url = self.minio_client.presigned_get_object(
+            MINIO_BUCKET, object_name, expires=timedelta(seconds=expiry)
+        )
+        return url
