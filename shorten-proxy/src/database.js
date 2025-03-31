84e9be7f6294b89e2a544792651df079f1b9bf14
PinJhih <isaacwang29@gmail.com>
2025-03-31 11:40:23 +0800

    Add database module to access SQLite

---
+++ shorten-proxy/src/database.js
@@ -0,0 +1,40 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("short_urls.db");
+
+db.run(`CREATE TABLE IF NOT EXISTS urls (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    file_name TEXT,
+    short_code TEXT UNIQUE,
+    long_url TEXT,
+    expires_at TIMESTAMP
+)`);
+
+function runQuery(query, params) {
+  return new Promise((resolve, reject) => {
+    db.run(query, params, function (err) {
+      if (err) reject(err);
+      else resolve(this);
+    });
+  });
+}
+
+function get(query, params) {
+  return new Promise((resolve, reject) => {
+    db.get(query, params, function (err, row) {
+      if (err) reject(err);
+      else resolve(row);
+    });
+  });
+}
+
+function getAll(query, params) {
+  return new Promise((resolve, reject) => {
+    db.all(query, params, function (err, rows) {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+}
+
+module.exports = { runQuery, get, getAll };
